c0069f421a274fb6ee9afe90fdf72bec6ce587d2
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-27 14:42:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
 
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-      binding.pry
+      course_offerings = doc.css(".post")
     end
 
     def get_courses
