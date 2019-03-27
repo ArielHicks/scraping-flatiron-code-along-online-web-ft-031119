d037d63bd5ec70165c20418a5c25271a4599f2d3
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-27 14:48:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,9 +15,9 @@ class Scraper
     course.description = post.css("p").text
   end
 
-    def get_courses
-      self.get_page.css(".post")
-    end
+  def get_courses
+    self.get_page.css(".post")
+  end
 
     def make_courses
       self.get_courses.each do |post|
